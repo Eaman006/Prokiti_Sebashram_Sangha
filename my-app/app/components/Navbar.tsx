7ce0b117dfdddfdaedd226caee491a132fbc1829
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2026-03-20 13:50:36 +0530

    Initialize Next.js application with TypeScript and Tailwind CSS setup
    - Created package.json with dependencies for Next.js, React, and Tailwind CSS.
    - Added postcss.config.mjs for Tailwind CSS integration.
    - Included SVG assets in the public directory for use in the application.
    - Configured TypeScript with tsconfig.json for strict type checking and module resolution.

---
+++ my-app/app/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-white text-black'>
+      <div className='text-3xl font-bold p-4'>
+        Prokriti Sebashram Sangha
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
