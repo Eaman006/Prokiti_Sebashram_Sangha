7d730dc3a1e2d6009ade47b9441d7c7873b49aab
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2026-03-21 15:16:46 +0530

    Refactor Navbar component layout for improved structure and styling

--- my-app/app/components/Navbar.tsx
+++ my-app/app/components/Navbar.tsx
@@ -3,8 +3,17 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='bg-white text-black'>
-      <div className='text-3xl font-bold p-4'>
-        Prokriti Sebashram Sangha
+      <div className='flex justify-between items-center'>
+        <div className='text-3xl font-bold p-4'>
+          Prokriti Sebashram Sangha
+        </div>
+        <div>
+          <ul className='flex gap-5'>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Contact us</li>
+          </ul>
+        </div>
       </div>
     </div>
   )
