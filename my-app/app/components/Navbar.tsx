ea90c81bdeed3d6afc634ffa4d5c33f7e32fd80c
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2026-03-21 21:52:39 +0530

    Enhance Navbar component with improved Store button styling and layout adjustments

--- my-app/app/components/Navbar.tsx
+++ my-app/app/components/Navbar.tsx
@@ -9,11 +9,11 @@ const Navbar = () => {
           Prokriti Sebashram Sangha
         </div>
         <div>
-          <ul className='flex gap-5 m-2'>
+          <ul className='flex gap-5 m-2 justify-center items-center'>
             <li className='cursor-pointer'>Home</li>
             <li className='cursor-pointer'>About us</li>
             <li className='cursor-pointer'>Contact us</li>
-            <div className='bg-purple-600'>Store</div>
+            <div className='bg-purple-600 p-2 rounded-xl cursor-pointer text-lg font-bold text-white hover:bg-purple-700 hover:active:bg-purple-500 transition duration-300 ease-in-out shadow-2xl'>Store</div>
           </ul>
 
         </div>
