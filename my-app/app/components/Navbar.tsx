697470465bbc7bcb8e969edc454e3af14886afab
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2026-03-22 17:31:52 +0530

    Add react-icons dependency and update Navbar with icons for navigation links

--- my-app/app/components/Navbar.tsx
+++ my-app/app/components/Navbar.tsx
@@ -1,6 +1,10 @@
 "use client"
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
+import { IoIosHome } from "react-icons/io";
+import { HiUserGroup } from "react-icons/hi2";
+import { IoCallSharp } from "react-icons/io5";
+import { MdLocalGroceryStore } from "react-icons/md";
 
 
 const Navbar = () => {
@@ -16,17 +20,17 @@ const Navbar = () => {
           Prokriti Sebashram Sangha
         </div>
         <div>
-          
+                    
           <ul className='flex gap-10 m-2 justify-center items-center'>
-            <li className={`cursor-pointer ${getActiveClass("/")}`}><Link href={"/"}>Home</Link></li>
-            <li className={`cursor-pointer ${getActiveClass("/about")}`}><Link href={"/about"}>About us</Link></li>
-            <li className={`cursor-pointer ${getActiveClass("/contact")}`}><Link href={"/contact"}>Contact us</Link></li>
+            <li className={`cursor-pointer ${getActiveClass("/")}`}><Link className='flex items-center' href={"/"}><IoIosHome />Home</Link></li>
+            <li className={`cursor-pointer ${getActiveClass("/about")}`}><Link className='flex items-center' href={"/about"}><HiUserGroup />About us</Link></li>
+            <li className={`cursor-pointer ${getActiveClass("/contact")}`}><Link className='flex items-center' href={"/contact"}> <IoCallSharp />Contact us</Link></li>
             
           </ul>
           
 
         </div>
-        <div className='bg-purple-600 m-2 px-4 py-2 rounded-xl cursor-pointer text-lg font-bold text-white hover:bg-purple-800 hover:active:bg-purple-500 transition duration-300 ease-in-out shadow-purple-600/20 shadow-md'>Store</div>
+        <div className='bg-purple-600 m-2 px-4 py-2 rounded-xl cursor-pointer text-lg font-bold text-white hover:bg-purple-800 hover:active:bg-purple-500 transition duration-300 ease-in-out shadow-purple-600/20 shadow-md flex items-center'><MdLocalGroceryStore />Store</div>
       </div>
     </div>
   )
