929179e13b78ed8b0e069f3fab8a4f37379574c2
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2026-03-22 17:40:10 +0530

    Enhance Navbar responsiveness by adjusting visibility of navigation links and Store button styling

--- my-app/app/components/Navbar.tsx
+++ my-app/app/components/Navbar.tsx
@@ -16,12 +16,12 @@ const Navbar = () => {
     
     <div className='bg-white text-black shadow-lg shadow-black/20 relative z-50'>
       <div className='flex justify-between items-center '>
-        <div className='text-3xl font-bold p-4 text-purple-600'>
+        <div className='md:text-3xl font-bold p-4 text-purple-600'>
           Prokriti Sebashram Sangha
         </div>
         <div>
                     
-          <ul className='flex gap-10 m-2 justify-center items-center'>
+          <ul className='hidden md:flex gap-10 m-2 justify-center items-center'>
             <li className={`cursor-pointer ${getActiveClass("/")}`}><Link className='flex items-center' href={"/"}><IoIosHome />Home</Link></li>
             <li className={`cursor-pointer ${getActiveClass("/about")}`}><Link className='flex items-center' href={"/about"}><HiUserGroup />About us</Link></li>
             <li className={`cursor-pointer ${getActiveClass("/contact")}`}><Link className='flex items-center' href={"/contact"}> <IoCallSharp />Contact us</Link></li>
@@ -30,7 +30,7 @@ const Navbar = () => {
           
 
         </div>
-        <div className='bg-purple-600 m-2 px-4 py-2 rounded-xl cursor-pointer text-lg font-bold text-white hover:bg-purple-800 hover:active:bg-purple-500 transition duration-300 ease-in-out shadow-purple-600/20 shadow-md flex items-center'><MdLocalGroceryStore />Store</div>
+        <div className='bg-purple-600 m-2 px-4 py-2 rounded-xl cursor-pointer md:text-lg font-bold text-white hover:bg-purple-800 hover:active:bg-purple-500 transition duration-300 ease-in-out shadow-purple-600/20 shadow-md flex items-center'><MdLocalGroceryStore />Store</div>
       </div>
     </div>
   )
