afb15d6187b1ea7766a3128f0252275aeb164b43
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2026-03-22 17:51:15 +0530

    Add mobile menu icon to Navbar and adjust Store button styling

--- my-app/app/components/Navbar.tsx
+++ my-app/app/components/Navbar.tsx
@@ -5,6 +5,7 @@ import { IoIosHome } from "react-icons/io";
 import { HiUserGroup } from "react-icons/hi2";
 import { IoCallSharp } from "react-icons/io5";
 import { MdLocalGroceryStore } from "react-icons/md";
+import { IoIosMenu } from "react-icons/io";
 
 
 const Navbar = () => {
@@ -19,8 +20,7 @@ const Navbar = () => {
         <div className='md:text-3xl font-bold p-4 text-purple-600'>
           Prokriti Sebashram Sangha
         </div>
-        <div>
-                    
+        <div>                    
           <ul className='hidden md:flex gap-10 m-2 justify-center items-center'>
             <li className={`cursor-pointer ${getActiveClass("/")}`}><Link className='flex items-center' href={"/"}><IoIosHome />Home</Link></li>
             <li className={`cursor-pointer ${getActiveClass("/about")}`}><Link className='flex items-center' href={"/about"}><HiUserGroup />About us</Link></li>
@@ -30,7 +30,8 @@ const Navbar = () => {
           
 
         </div>
-        <div className='bg-purple-600 m-2 px-4 py-2 rounded-xl cursor-pointer md:text-lg font-bold text-white hover:bg-purple-800 hover:active:bg-purple-500 transition duration-300 ease-in-out shadow-purple-600/20 shadow-md flex items-center'><MdLocalGroceryStore />Store</div>
+        <div className='bg-purple-600 m-2 px-4 py-2 rounded-xl cursor-pointer md:text-lg font-bold text-white hover:bg-purple-800 hover:active:bg-purple-500 transition duration-300 ease-in-out shadow-purple-600/20 shadow-md flex items-center text-sm'><MdLocalGroceryStore />Store</div>
+        <div className='md:hidden flex-col justify-center items-center text-sm hover:bg-purple-600 hover:active:bg-purple-600 cursor-pointer m-2 p-2'><IoIosMenu /> Menu</div>
       </div>
     </div>
   )
