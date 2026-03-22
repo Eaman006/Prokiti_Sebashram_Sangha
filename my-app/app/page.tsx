beb6177f5fc8d4922f2f0162ad51c431cc99f994
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2026-03-22 17:42:13 +0530

    Fix text size for better responsiveness in Home component

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="bg-white">
       <div className="md:flex mx-4 p-2 mt-10 justify-between">
-      <div className="text-black mx-auto text-5xl font-bold">Empowering Tribal Communities for a Better Future</div>
+      <div className="text-black mx-auto md:text-5xl font-bold text-xl">Empowering Tribal Communities for a Better Future</div>
       <Image src={"/banner.jpeg"} height={600} width={600} alt=""></Image>
       
       </div>           
